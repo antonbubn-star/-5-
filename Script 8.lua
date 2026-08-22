a16b2a4a4bd2f58e86970881bee47951293b4dd8
antonbubn-star <antonbubn@gmail.com>
2026-08-22 13:01:19 +0300

    Update Script 8.lua

--- Script 8.lua
+++ Script 8.lua
@@ -1,1 +1,3 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/deividcomsono/Obsidian/refs/heads/main/Library.lua"))()
+local Library = loadstring(game:HttpGet(repo .. "Library.lua"))()
+local SaveManager = loadstring(game:HttpGet(repo .. "addons/SaveManager.lua"))()
